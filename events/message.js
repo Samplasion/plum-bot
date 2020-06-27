c548ec905de043de3645e8d6de0b9f7187a7a4ad
Glitch (hello-express) <none>
2020-06-27 23:20:28 +0000

    💇☃️ Checkpoint ./events/message.js:657824/401

--- events/message.js
+++ events/message.js
@@ -2,10 +2,12 @@ module.exports = async (client, message) => {
   if (message.author.bot) return;
   if (message.command) {
     // assume it's a command
-    if (!client.global.has("log-channel") && client.channels.cache.fetch(client.global.has("log-channel"))) {
-      let channel = client.channels.cache.fetch(client.global.has("log-channel"));
+    if (client.global.has("log-channel") && client.channels.resolve(client.global.get("log-channel")) ) {
+      let channel = await client.channels.fetch(client.global.get("log-channel"));
+      console.log(channel);
       channel.send(`[${new Date()}] ${message.author.tag} ran the ${message.command.name} in #${message.channel.name}: ${message.cleanContent}`);
+    } else {
+      console.log(`[${new Date()}] ${message.author.tag} ran the ${message.command.name} in #${message.channel.name}: ${message.cleanContent}`);
     }
-    console.log(`[${new Date()}] ${message.author.tag} ran the ${message.command.name} in #${message.channel.name}: ${message.cleanContent}`);
   }
 }
