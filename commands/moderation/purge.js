e80ffc4a100a54fe54d62c07603f18eed774a2c4
Glitch (hello-express) <none>
2020-06-28 19:24:38 +0000

    ⛽️👸 Checkpoint ./commands/moderation/purge.js:657824/1910

---
+++ commands/moderation/purge.js
@@ -0,0 +1,32 @@
+const Command = require('../../classes/Command');
+const Embed = require("../../classes/Embed");
+
+module.exports = class PurgeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'purge',
+			group: 'util',
+			memberName: 'purge',
+			description: 'Purges a flooded channel.',
+			details: '(Get it? Flooded... purge... no?)',
+
+			args: [
+        {
+          type: "integer",
+          key: "amount",
+          label: "messages to purge (≤100)",
+          default: 100,
+          prompt: "how many messages do you want me to purge?",
+          validate: text => parseInt(text) > 0 && parseInt(text) <= 100,
+        }
+      ]
+		});
+	}
+
+	async run(msg, { amount }) {
+    let msgs = await msg.channel.messages.fetch({ limit: amount });
+    msg.channel.bulkDelete(msgs);
+    
+    this.client.utils.sendOkMsg(msg, `${amount} message${amount == 1 ? "" : "s"} were successfully purged.`);
+  }
+}
