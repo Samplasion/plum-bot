e781ba78d7bca3e8e05577a62f15eb6214300de9
Glitch (hello-express) <none>
2020-06-28 08:24:50 +0000

    🚩💠 Checkpoint ./commands/util/stats.js:657824/3087 ./package.json:657824/67 ./events/ready.js:657824/241 ./settings/index.js:657824/6 ./watch.json:657824/1

--- commands/util/stats.js
+++ commands/util/stats.js
@@ -1,13 +1,17 @@
 const Command = require("../../classes/Command");
+const { version } = require("discord.js");
+const moment = require("moment");
+const Embed = require("../../classes/Embed");
+require("moment-duration-format");
 
 module.exports = class EvalCommand extends Command {
   constructor(client) {
     super(client, {
       name: "stats",
       group: "util",
-      memberName: "reboot",
-      description: "Reboots the bot.",
-      details: "Only the bot owner(s) may use this command.",
+      memberName: "stats",
+      description: "Shows some stats of the bot.",
+      details: "Shows stats such as uptime, memory usage and Discord.js version.",
       ownerOnly: true,
 
       args: []
@@ -16,13 +20,18 @@ module.exports = class EvalCommand extends Command {
   
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const duration = moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-    message.channel.send(`= STATISTICS =
-  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  • Uptime     :: ${duration}
-  • Users      :: ${this.client.users.size.toLocaleString()}
-  • Servers    :: ${this.client.guilds.size.toLocaleString()}
-  • Channels   :: ${this.client.channels.size.toLocaleString()}
-  • Discord.js :: v${version}
-  • Node       :: ${process.version}`, {code: "asciidoc"});
+    
+    let embed = new Embed(this.client)
+      .setTitle("STATISTICS")
+      .addFields(
+        { name: "⚙️ Memory Usage", value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
+        { name: "⏱️ Uptime", value: duration, inline: true },
+        { name: "🌍 Users", value: this.client.users.cache.size.toLocaleString(), inline: true },
+        { name: "Servers", value: this.client.guilds.cache.size.toLocaleString(), inline: true },
+        { name: "Channels", value: this.client.channels.cache.size.toLocaleString(), inline: true },
+        { name: "🔢 Versions", value: `• Discord.js: v${version}\n• Node.js:    ${process.version}`, inline: true }
+      )
+    
+    message.channel.send(embed);
   }
 }
--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 function random(a, b = 0) {
     var max = Math.max(a, b),
         min = Math.min(a, b)
-    return ~(Math.random() * (max - min) + min)
+    return ~~(Math.random() * (max - min) + min)
 }
 
 module.exports = async client => {
@@ -19,10 +19,12 @@ module.exports = async client => {
   
   // Set "Playing" status
   const activityMessages = [`Help for ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users`, `Help for ${client.channels.cache.size.toLocaleString()} total channels`, `Type pl.help for a list of commands`];
-  client.user.setActivity(activityMessages[1], {type: 0});
   let i = random(activityMessages.length-1);
+  client.user.setActivity(activityMessages[i], {type: 0});
+  console.log("[LOG] Set status message:", activityMessages[i]);
   setInterval(() => {
     client.user.setActivity(activityMessages[i], {type: 0});
+    console.log("[LOG] Set status message:", activityMessages[i]);
     i++;
     i %= activityMessages.length;
   }, 120000);
