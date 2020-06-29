d03f134729f54f86ddf6dca45d61b570f426f0f2
Glitch (hello-express) <none>
2020-06-29 09:12:58 +0000

    💿🚣 Checkpoint ./events/guildMemberUpdate.js:657824/1530

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -8,13 +8,15 @@ module.exports = (client, oldMember, newMember) => {
     .setTitle("Member updated")
     .setAuthor(newMember.displayName, newMember.user.displayAvatarURL())
     .setThumbnail(newMember.user.displayAvatarURL())
+  if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
+    e.addInline("Old roles", oldMember["_roles"].sort((a, b) => oldMember.guild.roles.cache.get(a).name.localeCompare(oldMember.guild.roles.cache.get(b).name)).map(role => `<@&${role}>`).join(", ") || "None")
+    e.addInline("New roles", newMember["_roles"].sort((a, b) => newMember.guild.roles.cache.get(a).name.localeCompare(newMember.guild.roles.cache.get(b).name)).map(role => `<@&${role}>`).join(", ") || "None")
+  }
   Object.keys(d).forEach(k => {
     let r = k
-    if (r == "nickname") r = "displayName"
-    if (r == "_roles") {
-      e.addInline("Old roles", oldMember["_roles"].sort((a, b) => oldMember.guild.roles.cache.get(a).name.localeCompare(oldMember.guild.roles.cache.get(b).name)).map(role => `<@&${role}>`).join(", ") || "None")
-      e.addInline("New roles", newMember["_roles"].sort((a, b) => newMember.guild.roles.cache.get(a).name.localeCompare(newMember.guild.roles.cache.get(b).name)).map(role => `<@&${role}>`).join(", ") || "None")
-    } else {
+    if (["lastMessageID", "joinedTimestamp", "lastMessageChannelID"].some(t => r == t)) return;
+    if (r == "nickname") r = "displayName";
+    {
       e.addInline("Old " + Case.lower(k), oldMember[r] || "None")
       e.addInline("New " + Case.lower(k), newMember[r] || "None")
     }
