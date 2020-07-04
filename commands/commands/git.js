eb90ea6da9c55b092684af0573cea1cc4988a03d
Glitch (plum-bot) <none>
2020-07-04 11:18:39 +0000

    💈🏜 Checkpoint ./commands/commands/git.js:657824/9126

---
+++ commands/commands/git.js
@@ -0,0 +1,40 @@
+const util = require('util');
+const { MessageAttachment: Attachment } = require('discord.js');
+const Command = require('./../../classes/Command.js');
+const safeEval = require('safe-eval')
+
+module.exports = class EvalCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'git',
+			group: 'commands',
+			memberName: 'git',
+			description: 'Manages the mirroring of the repo to the bot\'s code and viceversa.',
+			details: 'Only the bot owner(s) may use this command.',
+      permLevel: 10,
+			args: [
+				{
+					key: 'argument',
+					prompt: 'what would you like to do?',
+					type: 'string',
+          oneOf: ["pull", "push", "latest"],
+          default: "lastest"
+				},
+        {
+          key: "args",
+          type: "string",
+          default: "",
+        }
+			]
+		});
+	}
+  
+  // hasPermission(msg) {
+    // if (!this.client.isOwner(msg.author)) return 'only the bot owner(s) may use this command.';
+    // return true;
+  // }
+
+	async run(message, { script }) {
+    
+  }
+}
