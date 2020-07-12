e5a28ddd129d9e1bc2d118d0caf8a54fafb8a131
Samplasion <dev@samplasion.js.org>
2020-07-12 09:38:17 +0200

    Log the member join/leave

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,6 +5,7 @@ function random(a, b = 0) {
 }
 
 module.exports = async (client, member) => {
+    // HELLO
     let guild = member.guild;
     if (!guild.config.data.welcomechan || 
         !guild.config.data.welcomemessage || 
@@ -19,4 +20,29 @@ module.exports = async (client, member) => {
         .split("{{server}}").join(guild.name)
         .split("{{user}}").join(member.displayName)
         .split("{{mention}}").join(`<@${member.user.id}>`));
+
+    // LOG
+    let name = member.user.username;
+    let owner = member.guild.members.get(member.guild.ownerID);
+
+    let logembed = this.client.utils.embed()
+        .setDescription(`This server now has ${member.guild.memberCount} members`)
+        .setThumbnail(member.guild.iconURL({format: 'png'}))
+        .addField('Account Age', member.user.createdAt)
+        .setFooter(`${member.user.tag} (#${member.user.id})`, member.user.displayAvatarURL({format: 'png'}));
+
+    let inviteregex = /(http(s)?:\/\/)?(discord(\.gg|app.com\/invite|.io))\/([a-zA-Z0-9]{1,15})?/gmi;
+    // eslint-disable-next-line no-useless-escape
+    let plsadd = /(pls\s+add|add\s+me)\s+([\.\:\/\w]{0,32})?\s+(\(tag\))\s+([\w#]{0,32})?/gmi;
+
+    if (name.match(inviteregex) || name.match(plsadd)) {
+        if (member.guild.me.hasPermission('BAN_MEMBERS')) {
+            if (name.match(inviteregex))	this.client.ban(member, "Invite link in username", owner);
+            if (name.match(plsadd))			this.client.ban(member, "Asking for friends in username", owner);
+        } else {
+            logembed.addField(':warning: Potential Malicious Account', 'I do not have the permissions nessesary to ban him')
+        }
+    }
+
+    guild.log(logembed);
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,7 +4,12 @@ function random(a, b = 0) {
     return ~~(Math.random() * (max - min) + min)
 }
 
+function isEmpty(variable) { //Function to check if value is really empty or not
+	return (variable && variable !== null && (variable.size || variable.length))
+}
+
 module.exports = async (client, member) => {
+    // GOODBYE
     let guild = member.guild;
     if (!guild.config.data.welcomechan || 
         !guild.config.data.leavemessage || 
@@ -19,4 +24,23 @@ module.exports = async (client, member) => {
         .split("{{server}}").join(guild.name)
         .split("{{user}}").join(member.displayName)
         .split("{{mention}}").join(`<@${member.user.id}>`));
+
+    // LOG
+    if (member.guild.partial) await member.guild.fetch();
+
+    let memberRemoveLogEmbed = this.client.utils.embed()
+        .setThumbnail(member.guild.iconURL({format: 'png'}))
+        .setDescription(`This server now has ${member.guild.memberCount} members`)
+        .setFooter(`${member.user.tag} [${member.id}]`, member.user.displayAvatarURL({format: 'png'}));
+
+    if (member.joinedAt)
+        memberRemoveLogEmbed.addField("Joined", member.joinedAt);
+
+    if (member.roles) {
+        let roles = member.roles.filter(role => role.id != member.guild.roles.everyone.id).map(r => `<@${r.id}>`).join(", ");
+        if(!isEmpty(roles))
+            memberRemoveLogEmbed.addField("Roles", roles);
+    }
+
+    guild.log(memberRemoveLogEmbed);
 }
