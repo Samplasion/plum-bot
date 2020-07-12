0a1cdaaa6d49e813463c9117dd75353f046cbb26
Samplasion <dev@samplasion.js.org>
2020-07-12 09:04:20 +0200

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -6,13 +6,16 @@ function random(a, b = 0) {
 
 module.exports = async function(client, member) {
     let guild = member.guild;
+    console.log(1, guild);
     if (!guild.config.data.welcomechan || 
         !guild.config.data.leavemessage || 
         !guild.config.data.leavemessage.length)
         return;
+    console.log(2);
     let channel = await client.channels.fetch(guild.config.data.welcomechan);
     if (!channel || !channel.sendable)
         return;
+        console.log(3);
     let messages = guild.config.data.leavemessage;
     let message = messages[random(messages.length)];
     channel.send(message
