f4e3b3093da4301b59be61e120b9370993e88686
Samplasion <dev@samplasion.js.org>
2020-07-14 00:04:26 +0200

    Add section

--- commands/commands/money.js
+++ commands/commands/money.js
@@ -1,4 +1,5 @@
 const Command = require('../../classes/Command');
+const { oneLine } = require("common-tags");
 
 module.exports = class MoneyCommand extends Command {
     constructor(client) {
@@ -22,14 +23,22 @@ module.exports = class MoneyCommand extends Command {
 
     run(message, { user }) {
         return message.say(
-            this.client.utils.fastEmbed("Money", `$${user.money}`, [[
-                "What can I do with money?",
-                "Money allows you to obtain special perks " + 
-                "without paying actual, real life money for it. To " + 
-                `check the available offers, use ${message.prefix}**shop**. ` +
-                "Keep in mind, though, that the shop is open only if " +
-                `you're in [Plum's Official Server](${this.client.options.invite})`
-            ]])
+            this.client.utils.fastEmbed("Money", `$${user.money}`, [
+                [
+                    "What can I do with money?",
+                    "Money allows you to obtain special perks " + 
+                    "without paying actual, real life money for it. To " + 
+                    `check the available offers, use ${message.prefix}**shop**. ` +
+                    "Keep in mind, though, that the shop is open only if " +
+                    `you're in [Plum's Official Server](${this.client.options.invite})`
+                ],
+                [
+                    "How can I earn money?",
+                    oneLine`You can earn more money by voting on the
+                    following lists. Each vote warrants $1 added to your balance.` + "\n\n" +
+                    `[Glenn Bot List](https://glennbotlist.xyz/bot/${this.client.user.id}/vote) | [botlist.space](https://botlist.space/bot/${this.client.user.id}/upvote) | [Bots for Discord](https://botsfordiscord.com/bot/${this.client.user.id}/vote)`
+                ]
+            ])
             .setAuthor(user.username, user.displayAvatarURL())
         );
     }
