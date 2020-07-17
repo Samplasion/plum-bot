082002bb47be471d9da70e3a908f6d8dfd510eca
Samplasion <dev@samplasion.js.org>
2020-07-17 18:02:41 +0200

    Whoops

--- commands/util/tags.js
+++ commands/util/tags.js
@@ -72,7 +72,7 @@ module.exports = class TagsCommand extends Command {
                 continue;
             }
 
-            msg.guild.tags.add(name, text);
+            msg.guild.tags.add(name.split(",,").join(","), text);
             added.push(name);
         }
 
