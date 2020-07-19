9207cd5843e03a65f31ee93ada007c626f2b0c74
Samplasion <dev@samplasion.js.org>
2020-07-19 11:37:33 +0200

    Next level at how many points

--- commands/util/points.js
+++ commands/util/points.js
@@ -1,32 +1,41 @@
 const Command = require('../../classes/Command');
 
 module.exports = class RandTextCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'points',
-      aliases: ['mypoints'],
-      group: 'util',
-      memberName: 'points',
-      description: 'Lets you see how many points you have.',
-      examples: ['perms'],
-      guildOnly: true,
-      args: [
-        {
-          key: "user",
-          type: "member",
-          prompt: "who do you want to see the permissions of?",
-          default: ""
-        }
-      ]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'points',
+            aliases: ['mypoints'],
+            group: 'util',
+            memberName: 'points',
+            description: 'Lets you see how many points you have.',
+            examples: ['perms'],
+            guildOnly: true,
+            args: [
+                {
+                    key: 'user',
+                    type: 'member',
+                    prompt: 'who do you want to see the permissions of?',
+                    default: '',
+                },
+            ],
+        });
+    }
 
-  run(message, { user }) {
-    // If pinged user, that. Otherwise message member
-    let member = user || message.member
-    return message.channel.send(
-      this.client.utils.fastEmbed("Points", `${member.points.data.points} points`, [["Level", member.points.data.level]])
-        .setAuthor(member.displayName, member.user.displayAvatarURL())
-    );
-  }
+    run(message, { user }) {
+        // If pinged user, that. Otherwise message member
+        let member = user || message.member;
+        let next = Math.ceil(Math.sqrt(member.points.data.points/100));
+        return message.channel.send(
+            this.client.utils
+                .fastEmbed(
+                    'Points', 
+                    `${member.points.data.points} points`, 
+                    [
+                        ['Level', member.points.data.level],
+                        ['Next level', `Level ${next} at ${(next ** 2) * 100} points`]
+                    ]
+                )
+                .setAuthor(member.displayName, member.user.displayAvatarURL())
+        );
+    }
 };
