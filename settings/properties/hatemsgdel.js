2f977fdbad496d833ecb6280d3f54543fde29ed1
Samplasion <dev@samplasion.js.org>
2020-07-28 20:07:38 +0200

    oof

--- settings/properties/hatemsgdel.js
+++ settings/properties/hatemsgdel.js
@@ -3,6 +3,6 @@ module.exports = {
     type: "bool",
     extendable: false,
     description: "Whether messages containing swears will be deleted by the bot. This option is " +
-        "automatically disabled it \"Re-send hateful message\" is enabled.",
+        "automatically disabled if \"Re-send hateful message\" is enabled.",
     category: "Anti-swear filter"
 } 
--- settings/properties/hateresponse.js
+++ settings/properties/hateresponse.js
@@ -4,6 +4,6 @@ module.exports = {
     extendable: false,
     long: true,
     description: "What the bot replies with when someone swears. This option is " +
-        "automatically disabled it \"Re-send hateful message\" is enabled.",
+        "automatically disabled if \"Re-send hateful message\" is enabled.",
     category: "Anti-swear filter"
 }
