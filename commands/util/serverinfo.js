ebdc6df28f345bcb0fc099603de0941a7e3f6e12
Samplasion <dev@samplasion.js.org>
2020-08-04 16:36:07 +0200

    Fix server info
    I think

--- commands/util/serverinfo.js
+++ commands/util/serverinfo.js
@@ -59,7 +59,7 @@ module.exports = class ServerInfoCommand extends Command {
   • ${e.message} | Text: **${guild.channels.cache.filter(c => c.type == "text").size}**
   • ${e.audio} | Voice: **${guild.channels.cache.filter(c => c.type == "voice").size}**
   • ${e.category} | Categories: **${guild.channels.cache.filter(c => c.type == "category").size}**`)
-            .addFields(this.client.utils.embedSplit(guild.emojis.cache.map(e => `${e}`).join(" ") || "No emojis", `${e.diamond} Emojis`, ", "))
+            .addFields(this.client.utils.embedSplit(guild.emojis.cache.map(e => `${e}`).join(" ") || "No emojis", `${e.diamond} Emojis`, " "))
             .addFields(this.client.utils.embedSplit(guild.roles.cache.filter(r => r.id != r.guild.id).sort((r1, r2) => r2.comparePositionTo(r1)).map(r => ext || ["plainroles", "pr"].some(f => msg.flags[f]) ? r.name : `<@&${r.id}>`).join(", ") || "No roles", `${e.users} Roles`, ", "),)
             .addField(`${e.boost} Server Level`, `${guild.premiumTier ? `Level ${guild.premiumTier}` : "Base level"} (${this.client.utils.plural(b, "boost")})`)
             .setFullFooter("ID: " + guild.id)
@@ -94,6 +94,6 @@ module.exports = class ServerInfoCommand extends Command {
 
         }
 
-        msg.channel.send(embed);
+        return msg.channel.send(embed);
     }
 };
